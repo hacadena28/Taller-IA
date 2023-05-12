cdc73af755719d82fbd41ddcd1993f2ee7195da3
Heli Cadena <100697841+hacadena28@users.noreply.github.com>
2023-05-12 05:10:15 -0500

    full

--- entrenamiento.py
+++ entrenamiento.py
@@ -24,98 +24,98 @@ u = matrizUmbrales(2)
 #**Comprobar los valores de los pesos
 w = matrizPesos(3,2)
 # print(b)
+def entrenas():
+    Yr = np.empty(salidas)
+    El = np.empty(salidas)
+    EpTotal=np.empty(patrones)
 
-Yr = np.empty(salidas)
-El = np.empty(salidas)
-EpTotal=np.empty(patrones)
+    for iteracion in range(0,numeroDeIteraciones):
+        print("iteracion: ",iteracion)
 
-for iteracion in range(0,numeroDeIteraciones):
-    print("iteracion: ",iteracion)
-
-    for patron in range(0,patrones):
-        
-        #**Patron falso
-        a = MatrizPatrones[patron]
-        print("patron: ",patron)
-        print(a)
-        print("\n")
-        print("\n")
-        print("\n")
-        print("\n")
-
-
-        Ep=0
-        for i in range(0,salidas): 
-            aux=0
-            print("valor de i:" ,i)
-            for j in range(0,entradas):
-                print("valor de j:",j)
-                print("valor de a[j]:",a[j])
-                print("valor de b[i][j]:",w[i][j])
-                aux+=(a[j])*(w[i][j])
-                print(aux)
-            print("umbral : ",i)
-            print(u[i])
-            print("salida: ",i)
-            Yr[i]=aux+u[i]
+        for patron in range(0,patrones):
             
-            print(Yr[i])
-            El[i] = s[i]-Yr[i]
-            print("error por salida: ",El[i])
-            #!agregar el valor absoluto 
-            if(El[i])<0:
-                El[i] = El[i]*-1
-                Ep+=El[i]
-            print("error: por patron: ",Ep)
-
-        Ep/=salidas
-
-        EpTotal[patron]=Ep
-        print(Yr)
-        print(El)
-        print("error: por patron: ",Ep)
-        
+            #**Patron falso
+            a = MatrizPatrones[patron]
+            print("patron: ",patron)
+            print(a)
+            print("\n")
+            print("\n")
+            print("\n")
+            print("\n")
 
 
-        #!Segunda parte del algoritmo
-        #**Modificacion de PESOS Y UMBRALES
-
-        print("Modificacion de pesos y umbrales")
-
-        print("Pesos viejos")
-        print(w)
-        print("Umbrales viejos")
-        print(u)
-        for i in range(0,salidas): 
-            aux = w[i]
-            print("valor de i:" ,i)
-            for j in range(0,entradas):
-                print("valor de j:",j)
-                print(w[i][j])
-                print(rataDeAprendizaje)
-                print(float(El[i]))
-                print(float(a[j]))
-                print(aux)
-                aux[j] = rataDeAprendizaje*El[i]*a[j]
-                print(aux[j])
-                print(aux)
-            w[i]= aux
-            u[i]+=rataDeAprendizaje*El[i]*1
+            Ep=0
+            for i in range(0,salidas): 
+                aux=0
+                print("valor de i:" ,i)
+                for j in range(0,entradas):
+                    print("valor de j:",j)
+                    print("valor de a[j]:",a[j])
+                    print("valor de b[i][j]:",w[i][j])
+                    aux+=(a[j])*(w[i][j])
+                    print(aux)
+                print("umbral : ",i)
+                print(u[i])
+                print("salida: ",i)
+                Yr[i]=aux+u[i]
                 
-        print("Pesos Nuevos")
-        print(w)
-        print("Umbrales Nuevos")
-        print(u)
+                print(Yr[i])
+                El[i] = s[i]-Yr[i]
+                print("error por salida: ",El[i])
+                #!agregar el valor absoluto 
+                if(El[i])<0:
+                    El[i] = El[i]*-1
+                    Ep+=El[i]
+                print("error: por patron: ",Ep)
+
+            Ep/=salidas
+
+            EpTotal[patron]=Ep
+            print(Yr)
+            print(El)
+            print("error: por patron: ",Ep)
+            
 
 
-    Erms = sum(EpTotal)/patrones
-    if(Erms <= errorMaximoPermitido):
-        print("\nError maximo alcanzado\n")
-        print("\n",Erms)
-        break
-    print("\n\n")
-    print("\n\n")
-    print("ERMS")
-    print(Erms)
-    print("\n\n")
-    print("\n\n")
+            #!Segunda parte del algoritmo
+            #**Modificacion de PESOS Y UMBRALES
+
+            print("Modificacion de pesos y umbrales")
+
+            print("Pesos viejos")
+            print(w)
+            print("Umbrales viejos")
+            print(u)
+            for i in range(0,salidas): 
+                aux = w[i]
+                print("valor de i:" ,i)
+                for j in range(0,entradas):
+                    print("valor de j:",j)
+                    print(w[i][j])
+                    print(rataDeAprendizaje)
+                    print(float(El[i]))
+                    print(float(a[j]))
+                    print(aux)
+                    aux[j] = rataDeAprendizaje*El[i]*a[j]
+                    print(aux[j])
+                    print(aux)
+                w[i]= aux
+                u[i]+=rataDeAprendizaje*El[i]*1
+                    
+            print("Pesos Nuevos")
+            print(w)
+            print("Umbrales Nuevos")
+            print(u)
+
+
+        Erms = sum(EpTotal)/patrones
+        if(Erms <= errorMaximoPermitido):
+            print("\nError maximo alcanzado\n")
+            print("\n",Erms)
+            break
+        print("\n\n")
+        print("\n\n")
+        print("ERMS")
+        print(Erms)
+        print("\n\n")
+        print("\n\n")
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from interface import Ui_MainWindow as UI
+import openpyxl
 
 class Aplicacion(QMainWindow):
     def __init__(self):
@@ -24,6 +25,8 @@ class Aplicacion(QMainWindow):
         self.ui.cBox_activacion.currentTextChanged.connect(self.toggle2)
         self.ui.btn_datos.clicked.connect(self.fileOpen)
         self.ui.btn_entrenar.clicked.connect(self.setiar)
+        self.ui.btn_simular.setEnabled(False)
+        self.ui.btn_simular.style()
         
         self.show()
 
@@ -50,11 +53,12 @@ class Aplicacion(QMainWindow):
             self.matrizPesos()
             self.matrizUmbrales()
             
+            
     def setiar(self):
-        self.iteraciones = self.ui.sBox_Iteraciones.value
-        self.rataAprendizaje = self.ui.dsBox_Rata.value
-        self.errorMaximo = self.ui.dsBox_EMP.value
-        print("prueba 1")
+        self.iteraciones = self.ui.sBox_Iteraciones.value()
+        self.rataAprendizaje = self.ui.dsBox_Rata.value()
+        self.errorMaximo = self.ui.dsBox_EMP.value()
+        self.entrenar()
         
     def toggle(self):
         if(self.ui.rbtn_per.isChecked()):
@@ -95,8 +99,109 @@ class Aplicacion(QMainWindow):
         self.matrizUmbrales = np.random.uniform(low=-1, high=1, size=(self.salida))
         self.matrizUmbrales = np.round(self.matrizUmbrales, decimals=2)
         print(self.matrizUmbrales)
-
+        q = self.matrizUmbrales
+        self.ui.tw_umb.setRowCount(1)
+        self.ui.tw_umb.setColumnCount(q.shape[0])
+        for i in range(q.shape[0]):
+            item = QTableWidgetItem(str(q[i]))
+            self.ui.tw_umb.setItem(0,i,item)
+        self.ui.tw_umb.resizeColumnsToContents()
+        self.ui.tw_umb.resizeRowsToContents()
+        self.ui.tw_umb.viewport().update()
     
+    def guardarPesos(matriz,nombre):
+        matriz= matriz.tolist()
+        workbook = openpyxl.Workbook()
+        sheet = workbook.active
+        for row in matriz:
+            sheet.append(row)
+            rutaPesos="archivosExcelGuardados/"+nombre+'.xlsx'
+            workbook.save(rutaPesos)
+        return rutaPesos
+    
+    def guardarUmbrales(matriz,nombre):
+        matriz=matriz.tolist()
+        workbook = openpyxl.Workbook()
+        sheet = workbook.active 
+        sheet.append(matriz)
+        rutaUmbrales="archivosExcelGuardados/"+nombre+'.xlsx'
+        workbook.save(rutaUmbrales)
+        return rutaUmbrales
+        
+    def entrenar(self):
+        self.Yr = np.zeros(self.salida)
+        self.El = np.zeros(self.salida)
+        self.EpTotal=np.zeros(self.patron)
+        for itera in range(self.iteraciones):
+            for patron in range(self.patron):
+                a =self.matrizEntrada[patron]
+                print(a)      
+                
+                Ep=0
+                for i in range(self.salida): 
+                    aux=0
+                  
+                    for j in range(self.entrada):
+                      
+                        aux+=(a[j])*(self.matrizPesos[i][j])
+                   
+                    self.Yr[i]=aux+self.matrizUmbrales[i]
+                    if(self.ui.cBox_activacion.currentIndex()==0 ):
+                        self.Yr[i] = self.Yr[i]
+                    else:
+                        if(self.Yr[i]>=0.5):
+                            self.Yr[i] = 1
+                        else:
+                            self.Yr[i] = 0
+                        print("si entra", self.Yr[i])
+                    
+                    self.El[i] = self.matrizSalida[i][i]-self.Yr[i]
+                   
+                    #!agregar el valor absoluto 
+                    if(self.El[i])<0:
+                        self.El[i] = self.El[i]*-1
+                        Ep+=self.El[i]
+                    print("error: por patron: ",Ep)
+
+                Ep/=self.salida
+
+                self.EpTotal[patron]=Ep
+                print(self.Yr)
+                print(self.El)
+                print("error: por patron: ",Ep)
+        
+                #!Segunda parte del algoritmo
+                #**Modificacion de PESOS Y UMBRALES
+
+                for i in range(self.salida): 
+                    aux = self.matrizPesos[i]
+                    print("valor de i:" ,i)
+                    for j in range(self.entrada):
+                     
+                        aux[j] = self.rataAprendizaje*self.El[i]*a[j]
+                       
+                    self.matrizPesos[i]= aux
+                    self.matrizUmbrales[i]+=self.rataAprendizaje*self.El[i]*1
+                        
+        
+    
+
+            self.Erms = sum(self.EpTotal)/self.patron
+            if(self.Erms <= self.errorMaximo):
+                print("\nError maximo alcanzado\n")
+                self.guardarPesos(self.matrizPesos,"Pesos")
+                self.guardarUmbrales(self.matrizUmbrales,"Umbrales")
+                self.ui.btn_simular.setEnabled(True)
+
+                print("ERMS")
+                print(self.Erms)
+                break
+           
+            print("ERMS")
+            print(self.Erms)
+           
+            
+            
 def main():
     windows = QApplication(sys.argv)
     ventana= Aplicacion()
